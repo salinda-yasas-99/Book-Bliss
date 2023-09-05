6c4a6c771775431674321d5e898bfc00218d21a0
salinda-yasas-99 <salindasym@gmail.com>
2023-09-05 20:14:00 +0530

    intial commit

---
+++ BackEnd/src/main/java/com/BookStore/BookBliss/BookBlissApplication.java
@@ -0,0 +1,13 @@
+package com.BookStore.BookBliss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookBlissApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookBlissApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/com/BookStore/BookBliss/BookBlissApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BookStore.BookBliss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookBlissApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
