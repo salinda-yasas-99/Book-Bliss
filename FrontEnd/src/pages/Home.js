c6830f041a696e853a99deef8543b86c120647df
salinda-yasas-99 <salindasym@gmail.com>
2023-09-06 23:31:07 +0530

    adding login,signup

--- FrontEnd/src/pages/Home.js
+++ FrontEnd/src/pages/Home.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Navbar from "../Componenets/Navbar";
 
 
 const Home  = () =>{
     return(
         <div className={"home"}>
+            <Navbar></Navbar>
             <h1>This is home page</h1>
+
         </div>
     )
 
