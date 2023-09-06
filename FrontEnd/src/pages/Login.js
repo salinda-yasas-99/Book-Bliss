cb29a7517b3941bf8ef9f93575cbc5c5bf1889f7
salinda-yasas-99 <salindasym@gmail.com>
2023-09-06 20:39:32 +0530

    adding frontend-dev branch

---
+++ FrontEnd/src/pages/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const LoginPage  = () =>{
+    return(
+        <div className={"LoginPage"}>
+            <h1>This is Login page</h1>
+        </div>
+    )
+
+}
+
+export default LoginPage;
---
+++ FrontEnd/src/pages/SingUp.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const SignUp  = () =>{
+    return(
+        <div className={"SignUp"}>
+            <h1>This is SignUp page</h1>
+        </div>
+    )
+
+}
+
+export default SignUp;
