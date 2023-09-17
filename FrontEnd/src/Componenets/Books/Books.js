1da6b6b1f4eb1d597198233e9d329e3cb4aa94c1
salinda-yasas-99 <salindasym@gmail.com>
2023-09-17 23:21:30 +0530

    adding cat,sub-cat select boxes

--- FrontEnd/src/Componenets/Books/Books.js
+++ FrontEnd/src/Componenets/Books/Books.js
@@ -1,102 +1,140 @@
 import React, { useState } from "react";
-import { Grid, InputAdornment, Input } from "@material-ui/core";
+import {
+    Grid,
+    InputAdornment,
+    Input,
+    Select,
+    MenuItem,
+    FormControl,
+    InputLabel,
+} from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
 import Book from "./Book/Book";
 import useStyles from "./BooksStyles";
 import Carousel from "react-bootstrap/Carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import logo1 from "../../assets/4.jpeg";
-import "./SingleBookView/bookView.css"
+import logo1 from "../../assets/Online-Bookstores-Better-than-Amazon-Featured.jpg";
+import "./SingleBookView/bookView.css";
 
-
-
-
-const Books = ({ books,onAddToCart }) => {
+const Books = ({ books, onAddToCart }) => {
     const classes = useStyles();
 
     const [searchTerm, setSearchTerm] = useState("");
+    const [selectedCategory, setSelectedCategory] = useState("");
+    const [selectedSubCategory, setSelectedSubCategory] = useState("");
+
+    const categories = ["Sinhala", "English"];
+    const subCategories = ["Novel", "Mystery", "Adventure", "Grade 10"];
 
     return (
         <main className={classes.mainPage}>
             <div className={classes.toolbar} />
             <Carousel fade infiniteLoop useKeyboardArrows autoPlay>
                 <Carousel.Item>
-                    <img className="d-block w-100" src={logo1} alt="slide" />
-                    <Carousel.Caption>
-                        <div className={classes.searchs}>
-                            <Input
-                                className={classes.searchb}
-                                type="text"
-                                placeholder="Which book are you looking for?"
-                                onChange={(event) => {
-                                    setSearchTerm(event.target.value);
-                                }}
-                                startAdornment={
-                                    <InputAdornment position="start">
-                                        <SearchIcon />
-                                    </InputAdornment>
-                                }
-                            />
-                        </div>
-                    </Carousel.Caption>
+                    <div className={classes.backgroundContainer}>
+                        <img className="d-block w-100" src={logo1} alt="slide" />
+                        <Carousel.Caption>
+                            <Grid container spacing={2} alignItems="center"  className={"main-container"}>
+                                <Grid item xs={6} sm={6} md={6} lg={6} xl={4} >
+                                    <div className={classes.searchs}>
+                                        <Input
+                                            className={classes.searchb}
+                                            type="text"
+                                            placeholder="Which book are you looking for?"
+                                            onChange={(event) => {
+                                                setSearchTerm(event.target.value);
+                                            }}
+                                            startAdornment={
+                                                <InputAdornment position="start">
+                                                    <SearchIcon />
+                                                </InputAdornment>
+                                            }
+                                        />
+                                    </div>
+                                </Grid>
+                                <Grid item xs={3} sm={3} md={3} lg={3} xl={4} >
+                                    <FormControl className={classes.formControl}>
+                                        <InputLabel>Category</InputLabel>
+                                        <Select
+                                            value={selectedCategory}
+                                            onChange={(event) => {
+                                                setSelectedCategory(event.target.value);
+                                            }}
+                                        >
+                                            {categories.map((category) => (
+                                                <MenuItem key={category} value={category}>
+                                                    {category}
+                                                </MenuItem>
+                                            ))}
+                                        </Select>
+                                    </FormControl>
+                                </Grid>
+                                <Grid item xs={3} sm={3} md={3} lg={3} xl={4} >
+                                    <FormControl className={classes.formControl}>
+                                        <InputLabel>Sub-category</InputLabel>
+                                        <Select
+                                            value={selectedSubCategory}
+                                            onChange={(event) => {
+                                                setSelectedSubCategory(event.target.value);
+                                            }}
+                                        >
+                                            {subCategories.map((subCategory) => (
+                                                <MenuItem key={subCategory} value={subCategory}>
+                                                    {subCategory}
+                                                </MenuItem>
+                                            ))}
+                                        </Select>
+                                    </FormControl>
+                                </Grid>
+                            </Grid>
+                        </Carousel.Caption>
+                    </div>
                 </Carousel.Item>
             </Carousel>
-
             {searchTerm === "" && (
                 <>
-                    <h3 className={classes.contentHeader}>FEATURED</h3>
-                    <Grid
-                        className={classes.contentFeatured}
-                        container
-                        justify="center"
-                        spacing={1}
-                    >
-                        {books.map((product) => (
-                            <>
-                                {product.length > 0 ? (
-                                    <Grid
-                                        className={classes.contentFeatured}
-                                        item
-                                        xs={6}
-                                        sm={5}
-                                        md={3}
-                                        lg={2}
-                                        id="pro"
-                                    >
-                                        <Book product={product} onAddToCart={onAddToCart} />
-                                    </Grid>
-                                ) : (
-                                    ""
-                                )}
-                            </>
+                    <Grid className={classes.content} container justify="center" spacing={5}>
+                        {books.map((singleBook) => (
+                            <Grid
+                                className={classes.content}
+                                item
+                                xs={12}
+                                sm={6}
+                                md={4}
+                                lg={3}
+                                id={singleBook.id}
+                                key={singleBook.id}
+                            >
+                                <Book book={singleBook} onAddToCart={onAddToCart} />
+                            </Grid>
                         ))}
                     </Grid>
                 </>
             )}
 
-            <Grid className={classes.content} container justify="center" spacing={5}>
-                {books
-                    .map((singleBook) => (
-
-                        <Grid
-                            className={classes.content}
-                            item
-                            xs={12}
-                            sm={6}
-                            md={4}
-                            lg={3}
-                            id={singleBook.id}
-                            key={singleBook.id}
-                        >
-                            <Book book={singleBook} onAddToCart={onAddToCart} />
-                        </Grid>
-                    ))}
-            </Grid>
+            {searchTerm !== "" && (
+                <>
+                    <Grid className={classes.content} container justify="center" spacing={5}>
+                        {books
+                            .filter((product) => {
+                                if (searchTerm === "") {
+                                    return product;
+                                } else if (
+                                    product.name.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+                                ) {
+                                    return product;
+                                }
+                            })
+                            .map((product) => (
+                                <Grid className={classes.content} item xs={12} sm={6} md={4} lg={3} id="pro">
+                                    <Book book={product} onAddToCart={onAddToCart} />
+                                </Grid>
+                            ))}
+                    </Grid>
+                </>
+            )}
         </main>
     );
 };
 
 export default Books;
-
-
-
