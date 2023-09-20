a10e950350a028144438ee3867e9851d5af612f1
salinda-yasas-99 <salindasym@gmail.com>
2023-09-20 11:04:10 +0530

    adding changes

--- BackEnd/src/main/java/com/BookStore/BookBliss/Controller/DemoController.java
+++ BackEnd/src/main/java/com/BookStore/BookBliss/Controller/DemoController.java
@@ -8,8 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/demo-controller")
-/*@CrossOrigin(origins = "*",allowedHeaders = "*")*/
-
+@CrossOrigin(origins = "http://localhost:3000")
 public class DemoController {
     @GetMapping
     @CrossOrigin(origins = "http://localhost:3000")
